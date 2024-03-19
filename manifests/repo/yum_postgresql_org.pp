262911ab1e3bf632b0e6e56e84efe83d8f5a4ed3
Michael Hodge <hodgem@wustl.edu>
2024-03-19 10:41:55 -0500

    Key/repo update from later version required (v10.0.3)  Cannot update version yet.

--- manifests/repo/yum_postgresql_org.pp
+++ manifests/repo/yum_postgresql_org.pp
@@ -1,18 +1,23 @@
-# PRIVATE CLASS: do not use directly
+# @api private
 class postgresql::repo::yum_postgresql_org inherits postgresql::repo {
   $version_parts   = split($postgresql::repo::version, '[.]')
   $package_version = "${version_parts[0]}${version_parts[1]}"
   $gpg_key_path    = "/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${package_version}"
 
-  file { $gpg_key_path:
-    source => 'puppet:///modules/postgresql/RPM-GPG-KEY-PGDG',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    before => Yumrepo['yum.postgresql.org']
+  $gpg_key_file = $facts['os']['release']['major'] ? {
+    '7'     => 'postgresql/RPM-GPG-KEY-PGDG-7',
+    default => 'postgresql/RPM-GPG-KEY-PGDG',
   }
 
-  if($::operatingsystem == 'Fedora') {
+  file { $gpg_key_path:
+    content => file($gpg_key_file),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    before  => Yumrepo['yum.postgresql.org'],
+  }
+
+  if($facts['os']['name'] == 'Fedora') {
     $label1 = 'fedora'
     $label2 = $label1
   } else {
@@ -20,15 +25,26 @@ class postgresql::repo::yum_postgresql_org inherits postgresql::repo {
     $label2 = 'rhel'
   }
   $default_baseurl = "https://download.postgresql.org/pub/repos/yum/${postgresql::repo::version}/${label1}/${label2}-\$releasever-\$basearch"
+  $default_commonurl = "https://download.postgresql.org/pub/repos/yum/common/${label1}/${label2}-\$releasever-\$basearch"
 
   $_baseurl = pick($postgresql::repo::baseurl, $default_baseurl)
+  $_commonurl = pick($postgresql::repo::commonurl, $default_commonurl)
 
   yumrepo { 'yum.postgresql.org':
     descr    => "PostgreSQL ${postgresql::repo::version} \$releasever - \$basearch",
     baseurl  => $_baseurl,
     enabled  => 1,
     gpgcheck => 1,
-    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${package_version}",
+    gpgkey   => "file://${gpg_key_path}",
+    proxy    => $postgresql::repo::proxy,
+  }
+
+  yumrepo { 'pgdg-common':
+    descr    => "PostgreSQL common RPMs \$releasever - \$basearch",
+    baseurl  => $_commonurl,
+    enabled  => 1,
+    gpgcheck => 1,
+    gpgkey   => "file://${gpg_key_path}",
     proxy    => $postgresql::repo::proxy,
   }
 
